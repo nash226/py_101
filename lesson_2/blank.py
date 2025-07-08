38b93561be3e37e42c4251cb069e57557b6cfb6a
Nazeer Shaikh <nazeer.shaikh54@gmail.com>
2025-07-07 21:08:15 -0400

    blank update

--- lesson_2/blank.py
+++ lesson_2/blank.py
@@ -1,4 +1,10 @@
-if False:
-    greeting = "hello world"
+messed_pokemon = 'BulbAsaur-ChaRMAndeR-pikaCHU-chariZard-SQuirtlE'
 
-print(greeting)
+list_1 = messed_pokemon.split('-')
+pokedex = []
+for pokemon in list_1:
+    pokedex.append(pokemon.title())
+
+pokedex.remove("Charizard")
+ 
+print(pokedex) # ['Bulbasaur', 'Charmander', 'Pikachu', 'Squirtle']
